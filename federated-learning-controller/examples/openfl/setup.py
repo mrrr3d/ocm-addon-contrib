6c939d9adb1c0c17c5f00c89933b5354ff45692a
mrrr61 <mrrr61@outlook.com>
2025-03-21 13:58:11 +0800

    add openfl-workspace

--- federated-learning-controller/examples/openfl/setup.py
+++ federated-learning-controller/examples/openfl/setup.py
@@ -71,7 +71,7 @@ setup(
             "openfl",
             # "openfl.*",
             "openfl-docker",
-            # "openfl-workspace",
+            "openfl-workspace",
             # "openfl-tutorials",
         )
     ),
